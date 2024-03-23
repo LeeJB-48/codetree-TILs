7430fcc2d21b7babef17c88617af813043dbddf0
LeeJB-48 <68397291+LeeJB-48@users.noreply.github.com>
2024-03-23 22:39:26 +0900

    Title: 진법 변환 6, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240323/진법 변환 6/base-conversion-6.py
@@ -0,0 +1,22 @@
+a,n,b = map(int,input().split())
+
+num_dict = {str(i):i for i in range(11)}
+rev_dict = {i:str(i) for i in range(11)}
+for char in list(map(chr, range(97, 123))):
+    num_dict[char] = 10 + ord(char) - ord('a')
+    rev_dict[10+ord(char)-ord('a')] = char
+# into 10진법
+
+rev = str(n)[::-1]
+num = sum([(a**i)*num_dict[rev[i]] for i in range(len(rev))])
+
+
+ret = ""
+while(num > 0):
+    num,na = divmod(num,b)
+    ret += rev_dict[na]
+
+    if num < b:
+        ret += rev_dict[num]
+        break
+print(ret[::-1])
