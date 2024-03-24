ece295f0babdc3a18243362f5405b119a10a770f
LeeJB-48 <68397291+LeeJB-48@users.noreply.github.com>
2024-03-25 05:49:13 +0900

    Title: 개발자들의 거리두기, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240325/개발자들의 거리두기/developers-keeping-distance.py
+++ 240325/개발자들의 거리두기/developers-keeping-distance.py
@@ -5,7 +5,7 @@ distances_btw = []
 min_R = 1000000
 
 for i in range(N-1):
-    if lst[i][1] == 0 or lst[i+1][1] == 0:
+    if lst[i][1] == 0 and lst[i+1][1] == 1 or lst[i][1] == 1 and lst[i+1][1] == 0:
         min_R = min(min_R,lst[i+1][0]-lst[i][0])
 
 min_R -= 1
@@ -31,4 +31,5 @@ for position,infected in lst:
             else:
                 prev = position
                 cnt += 1
+
 print(cnt)
